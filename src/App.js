e4a2ce4d4482027d21f1650067243e367f46a023
Yasir Akbar <yasir.akbar86@hotmail.com>
2020-11-18 11:58:14 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Login from "./components/login.jsx"
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Test</h1>
+    <Login />
+    </div>
+  );
+}
+
+export default App;
