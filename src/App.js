8a025cdf069cfa968846de32c31e28594a6161de
Yasir Akbar <yasir.akbar86@hotmail.com>
2020-11-18 13:58:01 +0000

    form 1.0 completed

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import Login from "./components/login.jsx"
 function App() {
   return (
     <div className="App">
-    <h1>Green Login Form</h1>
-    <Login />
+      <Login />
     </div>
   );
 }
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,42 +1,45 @@
 import React from "react";
+import "../App.css";
 import logo from "../images/logo.png";
 
 const Login = () => (
-  <div class="container">
-    <div class="row">
-      <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
-        <div class="card card-signin my-5">
-          <div class="card-body">
-              <span><img src={logo} height="50px" alt="logo"/></span>
-            <h5 class="card-title text-center">Operations studio</h5>
-            <p>Please enter your email below</p>
-            <form class="form-signin">
-              <div class="form-label-group">
-                <label for="inputEmail">Email address</label>
+  <div className="container">
+    <div className="row">
+      <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+        <div className="card card-box card-signin my-5">
+          <div className="card-body">
+            <span>
+              <img src={logo} alt="logo" />
+            </span>
+            <h4 className="card-title">Operations studio</h4>
+            <p className="email-text">Please enter your email below</p>
+            <form className="form-signin">
+              <div className="form-label-group email-address">
+                <label for="inputEmail">Email Address</label>
                 <input
                   type="email"
                   id="inputEmail"
-                  class="form-control"
+                  className="form-control"
                   required
                   autofocus
                 />
               </div>
 
-              <div class="custom-control custom-checkbox mb-3">
+              <div className="custom-control custom-checkbox mb-3">
                 <input
                   type="checkbox"
-                  class="custom-control-input"
+                  className="custom-control-input"
                   id="customCheck1"
                 />
-                <label class="custom-control-label" for="customCheck1">
-                  Remember this device
+                <label
+                  className="custom-control-label checkbox"
+                  for="customCheck1"
+                >
+                  <p className="span-text">Remember this device</p>
                 </label>
               </div>
-              <button
-                class="btn btn-lg btn-primary btn-block text-uppercase"
-                type="submit"
-              >
-                Sign in
+              <button className="btn btn-primary button border" type="submit">
+                Sign In
               </button>
             </form>
           </div>
